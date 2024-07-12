a620d9772bce9c1b5326e680469c14c7138242e4
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2024-07-12 16:37:13 +0200

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+
+import useStyles from "./styles";
+
+import { Actors, Movies, MovieInformation, NavBar, Profiles } from "./";
 
 const App = () => {
-  return <div>App is here.</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profiles />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Profiles/Profiles.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profiles = () => {
+  console.log("Profiles");
+  return (
+    <div>
+      <h1>Profiles</h1>
+    </div>
+  );
+};
+
+export default Profiles;
